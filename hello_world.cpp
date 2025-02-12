1a01142883d86fbbd08ae741e7680664f9f6cb0b
Min Wang <mingewang@gmail.com>
2025-02-11 22:21:08 -0600

    update hello msg

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,6 @@
 #include <iostream> // Include the input-output stream library
 
 int main() {
-    std::cout << "Hello, World!" << std::endl; // Print message to the console
+    std::cout << "Hello, USACO World!" << std::endl; // Print message to the console
     return 0; // Indicate successful execution
 }
